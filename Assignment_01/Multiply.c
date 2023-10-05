57583961137e3ef8a67d80ab0ed598f73d2fb4ec
nayemhasan45 <nayemhasan75907@gmail.com>
2023-10-05 20:58:55 +0100

    second problem done

---
+++ Assignment_01/Multiply.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    long long int first,second;
+    long long int total;
+    scanf("%lld \n%lld",&first,&second);
+    total=first*second;
+    // printf("%d%d",first,second);
+    printf("%lld",total);
+
+    return 0;
+}
