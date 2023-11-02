517120a0bdbddcbd1d5a0c099ae3636d18fd0dc4
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-02 14:22:21 +0000

    Do It problem solved of codeforces

---
+++ Mid-temr/Do_It.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while (t--)
+    {
+        int n;
+        scanf("%d",&n);
+        for (int i = 1; i <= n; i++)
+        {
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
