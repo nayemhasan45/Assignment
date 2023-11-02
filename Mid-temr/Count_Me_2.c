679ce2b864bbe4c8c3d863fdfe3a20f5d1acb546
nayemhasan45 <nayemhasan75907@gmail.com>
2023-11-02 13:28:12 +0000

    second problem done

---
+++ Mid-temr/Count_Me_2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[100001];
+    scanf("%s",&s);
+    int cnt=0;
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i]!='a'&&s[i]!='e'&&s[i]!='i'&&s[i]!='o'&&s[i]!='u')
+        {
+            cnt++;
+        }
+        
+    }
+    printf("%d",cnt);
+    
+    return 0;
+}
